c7ab2129ffec3811d74b17fb3c2b9cd6069d411d
DeAunte Hall <75832818+88-1982@users.noreply.github.com>
2021-07-16 15:15:51 -0700

    add second switch statment to js

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,20 @@
 //Business Logic
-function pizza(size,toppings){
+function pizza(size, toppings) {
   this.toppings = toppings;
   this.size = size;
   this.cost = 0;
 }
 
-pizza.prototype.determineCost = function(message){
-  switch (this.size){
+pizza.prototype.determineCost = function (message) {
+  switch (this.size) {
     case ("Large"):
       this.cost = 30 + this.toppings.length;
       message.push("Your total is $" + this.cost)
       break;
+    case ("medium"):
+      this.cost = 20 + this.toppings.length;
+      message.push("Your total is $" + this.cost)
+      break;
+
   }
 }
