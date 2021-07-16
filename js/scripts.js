53ca8b0e10d575cd9d4199f87064079d2e4e4c4b
DeAunte Hall <75832818+88-1982@users.noreply.github.com>
2021-07-16 15:08:52 -0700

    add first switch statment for large pizza to js

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,12 @@ function pizza(size,toppings){
   this.size = size;
   this.cost = 0;
 }
+
+pizza.prototype.determineCost = function(message){
+  switch (this.size){
+    case ("Large"):
+      this.cost = 30 + this.toppings.length;
+      message.push("Your total is $" + this.cost)
+      break;
+  }
+}
